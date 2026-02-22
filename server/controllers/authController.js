9064875414b885d49d840881cd04562b5601e42f
vjbarayuga <vjdbarayuga@gmail.com>
2026-02-22 15:55:12 +0800

    Default JWT expiry and validate secret

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,8 +3,12 @@ import User from '../models/User.js'
 
 // Generate JWT Token
 const generateToken = (id) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not set')
+  }
+
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: process.env.JWT_EXPIRE || '7d'
   })
 }
 
